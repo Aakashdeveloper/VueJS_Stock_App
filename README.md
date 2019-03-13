d196561bb2a58a2dce1c0a72b3f1ca21a1834f78
aakashDeveloper <ahanda205@gmail.com>
2019-03-13 11:40:42 +0530

    a

---
+++ README.md
@@ -0,0 +1,28 @@
+#App
+App is about buying and sell stock using Vue JS
+
+# Technologies
+Vue Js
+Vuex
+Bootstrap
+Firebase
+# vue-cli
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+firebaseUrl: https://vuejs-stock-trader-21674.firebaseio.com/
+
+
---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="text_home">
+        <h1>Trade or View your Portfolio</h1>
+        <h4>You may Save & Load your Data</h4>
+        <h6 >Click on 'End Day' to begin a New Day!</h6>
+        <hr>
+        <p >Your Funds: {{ funds | currency }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            funds() {
+                return this.$store.getters.funds;
+            }
+        }
+    }
+</script>
